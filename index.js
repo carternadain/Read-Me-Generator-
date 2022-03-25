5dba510cb6f3308baf5b315ec1fabc076c70c205
carternadain <cnadain@gmail.com>
2022-03-24 20:44:15 -0700

    add gitignore/test

---
+++ index.js
@@ -0,0 +1,20 @@
+// TODO: Include packages needed for this application
+// const inquirer=require('inquirer');
+// const fs =require('fs');
+// // TODO: Create an array of questions for user input
+// const questions = [];
+
+// // TODO: Create a function to write README file
+// function writeToFile(fileName, data) {}
+
+// // TODO: Create a function to initialize app
+// function init() {}
+
+// // Function call to initialize app
+// init();
+var message = 'Hello Node!';
+
+var sum = 5 + 3;
+
+console.log(message);
+console.log(sum);
